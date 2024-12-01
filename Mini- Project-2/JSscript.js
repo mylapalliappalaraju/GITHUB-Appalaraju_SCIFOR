e1c3706cdb6ea344f0c5d2857c2e7c243149b1e5
mylapalliappalaraju <Mylapalliappalaraju0703@gmail.com>
2024-12-02 03:05:22 +0530

    Add files via upload

---
+++ Mini- Project-2/JSscript.js
@@ -0,0 +1,106 @@
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn' )) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+                
+            }, 500)
+        }
+        else{
+            pageTurn.classList.add('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 + index;
+                
+            }, 500)
+
+        }
+
+    }
+})
+
+//contact me
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+    pages.forEach((pages, index) => {
+        setTimeout(() => {
+            page.classList.add('turn');
+
+            setTimeout(() => {
+                page.style.zIndex = 20 + index;
+            }, 500)
+
+        }, (index + 1) * 200 + 100)
+    })
+}
+
+//creating reverse index functions
+let totalpags = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+    pageNumber--;
+    if (pageNumber < 0) {
+        pageNumber = totalpags - 1;
+
+    }
+}
+
+//back profile button whren click
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(() => {
+                pages[pageNumber].style.zIndex = 10 + index;
+
+            })
+
+        }, (index + 1) * 200 + 100)
+    })
+}
+
+//opening page
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('book-page.page-left');
+
+//animation 
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100)
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+// page left or right
+setTimeout(() => {
+    pageLeft.style.zIndex = 20;
+}, 3200)
+
+
+
+//animation for all page
+pages.forEach((_, index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+
+        setTimeout(() => {
+            pages[pageNumber].style.zIndex = 10 + index;
+
+        })
+
+    }, (index + 1) * 200 + 2100)
+})
